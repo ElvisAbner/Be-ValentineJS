25929768bbee7576a6c5179809711604beaa1316
Elvis Estevez <124850509+ElvisAbner@users.noreply.github.com>
2024-02-12 22:08:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Yayyy!! Smadja, you make the happiest man on earth! <3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
